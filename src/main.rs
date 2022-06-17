658820fbc4f8d1b6e50e3fb60ca6487d6f7c746f
DOA454 <danielaudi75@gmail.com>
2022-06-17 14:59:58 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn adding() {
+    let num1 = 20;
+    let num2 = 25;
+    let new = num1 + num2; 
+    println!("This is the answer {}", new);   
+}
+
+fn main(){
+    println!("{} of {:b} do not understand VSCode", 9, 2);
+    adding();
+
+}
